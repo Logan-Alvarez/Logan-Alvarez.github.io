096c43070f4947e7fe2e7bd8a8b5e5f17a1f6351
Logan Alvarez <logan.alvarez616@gmail.com>
2022-02-21 13:43:01 -0500

    Finished Tools section

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -1,7 +1,39 @@
 function Tools() {
   return (
     <div className="Tools">
-      <h1>hey</h1>
+      <div className="Tool-containers">
+        <header>
+          <h2>Design</h2>
+        </header>
+        <p>
+          I strive to make simple yet elegant desings that give a pleasant user
+          interaction.
+        </p>
+        <h3>Things I enjoy designing</h3>
+        <p>UX, UI, Web, Mobile, Apps</p>
+        <h3>Design Tools</h3>
+        <ul className="tool-list">
+          <li>Figma</li>
+        </ul>
+      </div>
+      <div className="Tool-containers">
+        <header>
+          <h2>Front-End Development</h2>
+        </header>
+        <p>
+          I've only every created websites/applications from the ground up. I
+          enjoy a good challenge, and pushing the limits of what I can create.
+        </p>
+        <h3>Languages I use</h3>
+        <p>HTML,CSS, JavaScript</p>
+        <h3>Front-End Tools</h3>
+        <ul className="tool-list">
+          <li>Bootstrap</li>
+          <li>Google Fonts</li>
+          <li>Code-pen</li>
+          <li>Github</li>
+        </ul>
+      </div>
     </div>
   );
 }
